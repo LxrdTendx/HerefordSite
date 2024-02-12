9e55cdfb4da2120d3559e54a70b38b39aabc448e
Юрий Данилов <yuradan0903@gmail.com>
2024-02-12 17:51:12 +0300

    ~admin translate models

--- hereford/herefordsite/models.py
+++ hereford/herefordsite/models.py
@@ -3,58 +3,80 @@ import os
 from django.utils.timezone import now
 from django.urls import reverse
 class Region(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, unique=True, verbose_name='Регион')
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'Регион'
+        verbose_name_plural = 'Регионы'
 
 class CarouselImage(models.Model):
-    image = models.ImageField(upload_to='carousel_images/')
-    caption = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='carousel_images/', verbose_name='Изображение')
+    caption = models.TextField(blank=True, null=True, verbose_name='Подпись')
 
     def __str__(self):
-        return f"Carousel Image {self.id}"
+        return f"Карусель изображений {self.id}"
+
+    class Meta:
+        verbose_name = 'Изображение для карусели'
+        verbose_name_plural = 'Изображения для карусели'
 
 class ProductType(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, unique=True, verbose_name='Тип продукта')
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'Тип продукта'
+        verbose_name_plural = 'Типы продуктов'
 
 class SubProductType(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    product_type = models.ForeignKey(ProductType, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, unique=True, verbose_name='Подтип продукта')
+    product_type = models.ForeignKey(ProductType, on_delete=models.CASCADE, verbose_name='Тип продукта')
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'Подтип продукта'
+        verbose_name_plural = 'Подтипы продуктов'
+
 class Product(models.Model):
-    product_type = models.ForeignKey(ProductType, on_delete=models.CASCADE)
-    sub_product_type = models.ForeignKey(SubProductType, on_delete=models.CASCADE, null=True, blank=True)
-    region = models.ForeignKey(Region, on_delete=models.SET_NULL, null=True, blank=True)
-    farm = models.CharField(max_length=255, blank=True, null=True)
-    contact_phone = models.CharField(max_length=20, blank=True, null=True)
-    details = models.TextField(blank=True, null=True)
-    product_photo = models.ImageField(upload_to='products_photos/', blank=True, null=True)
-    age = models.CharField(max_length=255, blank=True, null=True)
-    price = models.CharField(max_length=255, blank=True, null=True)
+    product_type = models.ForeignKey(ProductType, on_delete=models.CASCADE, verbose_name='Тип продукта')
+    sub_product_type = models.ForeignKey(SubProductType, on_delete=models.CASCADE, null=True, blank=True, verbose_name='Подтип продукта')
+    region = models.ForeignKey(Region, on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Регион')
+    farm = models.CharField(max_length=255, blank=True, null=True, verbose_name='Ферма')
+    contact_phone = models.CharField(max_length=20, blank=True, null=True, verbose_name='Контактный телефон')
+    details = models.TextField(blank=True, null=True, verbose_name='Детали')
+    product_photo = models.ImageField(upload_to='products_photos/', blank=True, null=True, verbose_name='Фото продукта')
+    age = models.CharField(max_length=255, blank=True, null=True, verbose_name='Возраст')
+    price = models.CharField(max_length=255, blank=True, null=True, verbose_name='Цена')
 
     def __str__(self):
         return f"{self.product_type.name} - {self.sub_product_type.name if self.sub_product_type else ''}"
 
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
 
 class Farm_point(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    contact_phone = models.CharField(max_length=20, null=False)
-    address = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True)
-    region = models.ForeignKey(Region, on_delete=models.SET_NULL, null=True, blank=True)
-    website = models.CharField(max_length=50, null=True)
-    pdf_page = models.IntegerField(null=True, blank=True)  # новое поле для номера страницы PDF
+    name = models.CharField(max_length=255, unique=True, verbose_name='Название')
+    contact_phone = models.CharField(max_length=20, verbose_name='Контактный телефон')
+    address = models.CharField(max_length=255, verbose_name='Адрес')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    region = models.ForeignKey(Region, on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Регион')
+    website = models.CharField(max_length=50, null=True, verbose_name='Веб-сайт')
+    pdf_page = models.IntegerField(null=True, blank=True, verbose_name='Страница PDF')  # обновлено
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'Фермерская точка'
+        verbose_name_plural = 'Фермерские точки'
 
 def news_image_path(instance, filename):
     # Форматируем дату в нужный формат, например '07-02-2024'
@@ -64,15 +86,15 @@ def news_image_path(instance, filename):
     return os.path.join('news', date_str, filename)
 
 class News_Page(models.Model):
-    title = models.CharField(max_length=70, unique=True)
-    text = models.TextField(max_length=6000, blank=True, null=True)
-    date = models.DateField()
-    photo1 = models.ImageField(upload_to=news_image_path, blank=True, null=True)
-    photo2 = models.ImageField(upload_to=news_image_path, blank=True, null=True)
-    photo3 = models.ImageField(upload_to=news_image_path, blank=True, null=True)
-    photo4 = models.ImageField(upload_to=news_image_path, blank=True, null=True)
-    photo5 = models.ImageField(upload_to=news_image_path, blank=True, null=True)
-    photo6 = models.ImageField(upload_to=news_image_path, blank=True, null=True)
+    title = models.CharField(max_length=70, unique=True, verbose_name='Заголовок')
+    text = models.TextField(max_length=6000, blank=True, null=True, verbose_name='Текст')
+    date = models.DateField(verbose_name='Дата')
+    photo1 = models.ImageField(upload_to=news_image_path, blank=True, null=True, verbose_name='Фото 1')
+    photo2 = models.ImageField(upload_to=news_image_path, blank=True, null=True, verbose_name='Фото 2')
+    photo3 = models.ImageField(upload_to=news_image_path, blank=True, null=True, verbose_name='Фото 3')
+    photo4 = models.ImageField(upload_to=news_image_path, blank=True, null=True, verbose_name='Фото 4')
+    photo5 = models.ImageField(upload_to=news_image_path, blank=True, null=True, verbose_name='Фото 5')
+    photo6 = models.ImageField(upload_to=news_image_path, blank=True, null=True, verbose_name='Фото 6')
 
     def get_photos(self):
         photos = [self.photo1, self.photo2, self.photo3, self.photo4, self.photo5, self.photo6]
@@ -80,3 +102,7 @@ class News_Page(models.Model):
 
     def get_absolute_url(self):
         return reverse('news_detail', args=[str(self.id)])
+
+    class Meta:
+        verbose_name = 'Страница новостей'
+        verbose_name_plural = 'Страницы новостей'
